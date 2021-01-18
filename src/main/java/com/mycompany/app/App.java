80e89298cfe714c77bb3f26f92e6b72ef243b268
sudheeramya <44565511+sudheeramya@users.noreply.github.com>
2021-01-18 22:35:29 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         System.out.println(new App().getMessage());
     }
 
