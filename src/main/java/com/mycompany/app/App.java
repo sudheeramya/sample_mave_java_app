d7e4fc023e76ad17b8e56077adb8ad06130a0674
sudheeramya <44565511+sudheeramya@users.noreply.github.com>
2021-01-18 22:49:39 +0530

    Update App.java
    public static void main(String[] args) 
            System.out.println(new App().getMessage());

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,10 +10,7 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) 
-        System.out.println(new App().getMessage());
     
-
     private final String getMessage() {
         return message;
     }
