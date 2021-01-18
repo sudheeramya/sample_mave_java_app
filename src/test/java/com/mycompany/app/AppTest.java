30a808e57e297d74f6a900b1de2e7b89556ddfcf
sudheeramya <44565511+sudheeramya@users.noreply.github.com>
2021-01-18 22:48:15 +0530

    Update AppTest.java
    import org.junit.Before;
    import org.junit.Test;
    import org.junit.After;
    import static org.junit.Assert.*;

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,10 +2,6 @@
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
